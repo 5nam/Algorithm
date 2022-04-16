eff2fe21769fffab10d51aa924f112dec946723e
5nam <ohnam90@gmail.com>
2022-04-16 23:17:02 +0900

    알고리즘 문제 최댓값 구하기

---
+++ class_01/Main_2562.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int[] num = new int[9];
+		for(int i = 0; i<9; i++) {
+			num[i] = sc.nextInt();
+		}
+		
+		int big = num[0];
+		int index = 0;
+		for(int j = 1; j<9; j++) {
+			if(big < num[j]) {
+				big = num[j];
+				index = j;
+			}
+		}
+		System.out.println(big);
+		System.out.println(index+1);
+	}
+}
