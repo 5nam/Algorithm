481ef737d7ce57fd023ccbc27a16f5a78cb3867c
5nam <86754153+5nam@users.noreply.github.com>
2022-04-17 21:24:38 +0900

    알고리즘 문제 풀이
    알람 시계 문제 풀이

---
+++ class_01/Main_2884.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int H, M;
+		H = sc.nextInt();
+		M = sc.nextInt();
+		int M_num = M-45;
+		
+		if(H == 0) {
+			if(M_num < 0) {
+				H = 23;
+				M = 60 + M_num;
+			}
+			else if(M_num >= 0) {
+				M = M_num;
+			}
+		}
+		else if(H != 0) {
+			if(M_num < 0) {
+				H--;
+				M = 60 + M_num;
+			}
+			else if(M_num >= 0) {
+				M = M_num;
+			}
+		}
+		
+		System.out.println(H + " " + M);
+		sc.close();
+	}
+}
