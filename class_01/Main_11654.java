9313428020c2fb2ba915aacd6ac55ea19fd5f2ad
5nam <86754153+5nam@users.noreply.github.com>
2022-04-20 23:08:41 +0900

    아스키코드 문제

---
+++ class_01/Main_11654.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String s = sc.next();
+		char m = 0;
+		for(int i = 0; i<s.length(); i++) {
+			m = s.charAt(i);
+		}
+		System.out.println((int)m);
+		sc.close();
+	}
+}
