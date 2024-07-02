08c3f897bfa038cf8bd41ded8d40c0908ff8782e
5nam <ohnam90@gmail.com>
2024-07-02 14:08:15 +0900

    [refactor] : 책 코드 참고하여 변경 - 내 코드 : 시간 복잡도 O(K), 공간 복잡도 O(1), 가독성 및 유지 보수 측면에서 코드 중복이 있고, 코드가 길어져서 좋지 않음 - 책 코드 : 시간 복잡도 O(K), 공간 복잡도 O(K), 가독성 및 유지 보수 측면에서 코드 중복이 없고, 코드가 짧아서 좋음 결론 : 책 코드로 수정, 공간 복잡도가 O(1), O(K) 로 차이가 있지만 실제로 메모리는 매우 적기 때문에 크게 문제가 되지 않음. 가독성 및 유지보수 측면에서 좋은 코드를 선택

--- greedy/example-3-1.py
+++ greedy/example-3-1.py
@@ -1,21 +1,10 @@
-money = int(input('금액을 입력하세요 : '))
+n = 1260
 count = 0
 
-if money % 10 != 0:
-    print('N 은 항상 10의 배수')
-    exit
+coin_types = [500, 100, 50, 10]
 
-if money >= 500:
-    count += money // 500
-    money = money % 500
-if money >= 100:
-    count += money // 100
-    money = money % 100
-if money >= 50:
-    count += money // 50
-    money = money % 50
-if money >= 10:
-    count += money // 10
-    money = money % 10
+for coin in coin_types:
+	count += n // coin
+	n %= coin
 
 print(count)
