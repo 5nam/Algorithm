867087a479034f8061fa68b2462780352401bb7d
5nam <ohnam90@gmail.com>
2024-07-02 19:27:35 +0900

    [refactor] : 책 풀이로 변경 - 내 풀이 : 시간 복잡도 O(n log n + m), 공간 복잡도 O(n) - 책 풀이 : 시간 복잡도 O(n log n), 공간 복잡도 O(n) - 더하는 수를 나열해보고, 그 안에서 규칙성을 찾아서 수식을 통해 훨씬 간결하게 풀이

--- greedy/bignumber-rule.py
+++ greedy/bignumber-rule.py
@@ -1,26 +1,15 @@
-import time
+n, m, k = map(int, input().split())
+data = list(map(int, input().split()))
 
-n, m, k = map(int, input().split(' '))
+data.sort() # 입력받은 수 정렬
+first = data[n-1]
+second = data[n-2]
 
-if k > m:
-    exit
+count = int(m/(k+1)) * k
+count += m % (k+1)
 
-#2. 1차원 배열 입력받기 = 정수형 리스트로 저장
-numbers = list(map(int, input().split(' ')))
-
-numbers.sort(reverse=True)
-
-count = 0
 result = 0
-
-for _ in range(m):
-
-    if count == k:
-       result += numbers[1]
-       count = 0
-       continue
-
-    result += numbers[0]
-    count += 1
+result += (count) * first
+result += (m - count) * second
 
 print(result)
