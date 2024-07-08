c6d0c56e29280495e61b9f12a485596f4201b54b
5nam <ohnam90@gmail.com>
2024-07-09 00:03:46 +0900

    엘리스 알고리즘 문제 첫 번째 풀이

---
+++ elice/01.py
@@ -0,0 +1,54 @@
+num = '999989'
+num_list = list(num)
+
+def solution(num, num_list):
+    if(len(num_list) == 1) :
+        return num 
+    elif (len(num_list) == 2) :
+        num_list.reverse()
+        num2 = strListToInt(num_list)
+        
+        if(int(num) >= num2) :
+            return num
+        else :
+            return num2
+    
+    for _ in range(-1, -len(num_list)-1, -1):
+        num_list = recursion(-2, -1, num_list)
+        result = strListToInt(num_list)
+    
+    return result
+    
+
+def recursion(left_index, right_index, num_list):
+    if(len(num_list)*(-1) > left_index):
+        return strListToInt(num_list)
+    if(int(num_list[left_index]) < int(num_list[right_index])):
+        if(strListToInt(num_list) > strListToInt(num)):
+            return num_list
+        temp = num_list[left_index]
+        num_list[left_index] = num_list[right_index]
+        num_list[right_index] = temp
+        return num_list
+        # recursion(left_index, right_index, num_list)     
+
+    elif(int(num_list[left_index]) >= int(num_list[right_index])):
+        if(strListToInt(num_list) > strListToInt(num)):
+            temp_list = list(num_list)
+            temp = num_list[left_index]
+            num_list[left_index] = num_list[right_index]
+            num_list[right_index] = temp
+            if(strListToInt(temp_list) < strListToInt(num_list) or int(num) >= strListToInt(num_list)):
+                num_list = list(temp_list)
+                return num_list
+            return num_list   
+
+    recursion(left_index-1, right_index, num_list)
+
+def strListToInt(num_list):
+    num_list = ''.join(num_list)
+    return int(num_list)
+    
+    
+
+print(solution(num, num_list))
