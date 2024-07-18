fe6d07ffa7a90d8659dd30c8bca3af81c237b067
5nam <ohnam90@gmail.com>
2024-07-18 12:39:31 +0900

    풀이 참고

--- list/07.py
+++ list/07.py
@@ -1,31 +1,34 @@
 def solution(dirs):
     direction = list(dirs)
-    coordinate = [0,0,0]
+
+    dx = [0, 0, 1, -1]
+    dy = [1, -1, 0, 0]
+
+    x, y = 0, 0
+
     route = []
-    n = len(direction)
     
     # 방문한 좌표랑 방향 저장하기
-    for i in range(n):
-        if coordinate[1] < 5 and direction[i] == 'U':
-            coordinate[1] += 1
-            coordinate[2] = 0
-        elif coordinate[1] > -5 and direction[i] == 'D':
-            coordinate[1] -= 1
-            coordinate[2] = 1
-        elif coordinate[0] < 5 and direction[i] == 'R':
-            coordinate[0] += 1
-            coordinate[2] = 2
-        elif coordinate[0] > -5 and direction[i] == 'L':
-            coordinate[0] -= 1
-            coordinate[2] = 3
-        
-        # 이렇게 짠 이유는 파이썬은 리스트를 append 하면, 그 리스트의 주소값을 저장하기 때문에
-        # 하나의 리스트를 계속 이용해서 연산할 경우 값이 같이 변하므로!
-        route.append([coordinate[0], coordinate[1], coordinate[2]])
-    
-    # route 중복 제거 및 개수 반환
-    unique_set = set(map(tuple, route))
-    
-    return len(unique_set)
+    for value in direction:
+        if value == 'U':
+            nx, ny = x + dx[0], y + dy[0]
+        elif value == 'D':
+            nx, ny = x + dx[1], y + dy[1]
+        elif value == 'R':
+            nx, ny = x + dx[2], y + dy[2]
+        elif value == 'L':
+            nx, ny = x + dx[3], y + dy[3]
 
-print(solution("LULLLLLLU"))
+        if -5 <= nx <= 5 and -5 <= ny <= 5:
+            px = x
+            py = y
+            x = nx
+            y = ny
+            if [px, x, py, y] not in route and [x, px, y, py] not in route:
+                route.append([px, x, py, y])
+        else:
+            nx = x
+            ny = y
+    
+    
+    return len(route)
