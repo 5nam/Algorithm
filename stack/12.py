76be99d53e732d6bfeb2d95b056a809497aac37f
5nam <ohnam90@gmail.com>
2024-07-26 16:17:13 +0900

    O(N) 풀이 설명 보고 작성한 코드 중에 틀린 부분 수정 - 스택이 비지 않을 경우까지만 while 문을 돌려야 하는데, 그 조건을 안 넣어서 런타임 에러가 났음

--- stack/12.py
+++ stack/12.py
@@ -1,18 +1,19 @@
 def solution(prices):
-    p_num = len(prices)
+    n = len(prices)
+    result = [0] * n
+
     stack = [0]
-    result = [0] * p_num
-
-    for i in range(1, p_num):
-        if prices[stack[-1]] > prices[i]:
-            while prices[stack[-1]] > prices[i]:
-                result[stack.pop()] = i - stack[-1]
+    for i in range(1, n):
+        # 스택이 비어있지 않고, 이전 가격이 현재 가격보다 클 경우 가격이 하락했다고 볼 수 있음
+        while stack and prices[i] < prices[stack[-1]]:
+            j = stack.pop()
+            result[j] = i - j
         stack.append(i)
-    
-    s_num = len(stack)
 
-    for i in range(s_num):
-        result[stack.pop()] = p_num - 1 - stack[-1]
+    # 스택에 남아있는 가격들은 끝까지 가격 하락이 없는 경우임
+    while stack:
+        j = stack.pop()
+        result[j] = n - 1 - j
 
     return result
 
