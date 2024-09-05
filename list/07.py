06e288b3ddd8e07dd908d0933fbc50a8bb53f269
5nam <ohnam90@gmail.com>
2024-09-05 22:35:37 +0900

    다시 풀어봄 : 시간 복잡도 O(N^2)

--- list/07.py
+++ list/07.py
@@ -1,14 +1,62 @@
 def solution(dirs):
-    route = set()
-    direction = {'U':(0, 1), 'D':(0, -1),'R':(1, 0), 'L':(-1, 0)}
+    
+    dirs = list(dirs)
+    now = [0, 0]
+    route = []
+    count = 0
+    
+    for dir in dirs:
+        if dir == 'U':
+            if now[1] == 5:
+                continue
 
-    x, y = 0, 0
+            next_now = [now[0], now[1] + 1]
+            new_route = [now, next_now]
+            new_route_reverse = [next_now, now]
+            
+            now = next_now
 
-    for value in dirs:
-        nx, ny = x + direction[value][0], y + direction[value][1]
-        if -5 <= nx <= 5 and -5 <= ny <= 5:
-            route.add((x, y, nx, ny))
-            route.add((nx, ny, x, y))
-            x, y = nx, ny
+        elif dir == 'D':
+            if now[1] == -5:
+                continue
+            
+            next_now = [now[0], now[1] - 1]
+            new_route = [now, next_now]
+            new_route_reverse = [next_now, now]
+            
+            now = next_now
+        elif dir == 'R':
+            if now[0] == 5:
+                continue
+            
+            next_now = [now[0] + 1, now[1]]
+            new_route = [now, next_now]
+            new_route_reverse = [next_now, now]
+            
+            now = next_now
+            
+        elif dir == 'L':
+            if now[0] == -5:
+                continue
 
-    return len(route)//2
+            next_now = [now[0] - 1, now[1]]
+            new_route = [now, next_now]
+            new_route_reverse = [next_now, now]
+            
+            now = next_now
+        
+        if new_route not in route or new_route_reverse not in route:
+            count += 1
+            route.append(new_route)
+            route.append(new_route_reverse)
+            
+    
+    return count
+
+
+# dirs = "ULURRDLLU"
+dirs = "LULLLLLLU"
+# dirs = "LLLLRRRRR"
+
+
+print(solution(dirs))
