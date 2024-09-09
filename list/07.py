d2ae84f9056f72555a55ec8b54f9ed4a466d8aff
5nam <ohnam90@gmail.com>
2024-09-09 21:11:32 +0900

    틀린 풀이 수정:

--- list/07.py
+++ list/07.py
@@ -11,12 +11,12 @@ def solution(dirs):
 
         # 현재 상태에서 이미 다음 위치로 이동시킨 nx, ny 값이 5보다 작거나 같으면 현재 이동해도 되는 상태인 것
         if abs(nx) <= 5 and abs(ny) <= 5:
-            route.add(x, y, nx, ny)
-            route.add(dx, dy, x, y)
+            route.add((x, y, nx, ny))
+            route.add((nx, ny, x, y))
 
             x, y = nx, ny       
     
-    return route // 2
+    return len(route) // 2
 
 
 # dirs = "ULURRDLLU"
