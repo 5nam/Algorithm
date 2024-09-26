706a34f63959c4192110082b0b7f26f09d5ebcfb
5nam <ohnam90@gmail.com>
2024-09-26 11:39:35 +0900

    알고리즘 파일 정리

---
+++ list/Main11720.java
@@ -0,0 +1,24 @@
+package list;
+
+import java.util.Scanner;
+
+/**
+ * N 은 100, 시간복잡도 O(N)
+ */
+public class Main11720 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int number = Integer.parseInt(sc.nextLine());
+        String[] numbers = sc.nextLine().split("");
+        int result = 0;
+
+        for (int i = 0; i<number; i++) {
+            result += Integer.parseInt(numbers[i]);
+        }
+
+        System.out.println(result);
+
+        sc.close();
+    }
+}
