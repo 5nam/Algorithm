ed8a2dc6f23006062174dac5f1316f71c323c24e
5nam <ohnam90@gmail.com>
2024-09-26 12:21:45 +0900

    1546 리팩터링 1. 이전 풀이는 최댓값 구하는 것과 총합 구하는 것이 나눠져있었음. 그래서 약 2N 번 연산을 수행 2. 평균을 내는 수식을 활용하여 (점수의 총합) / M * 100 / 3 도 같은 결과를 내는 것을 활용하여 N 번만 연산 수행

--- list/Main1546.java
+++ list/Main1546.java
@@ -3,7 +3,7 @@ package list;
 import java.util.Scanner;
 
 /**
- * 시간복잡도 : O(N)
+ * 시험 과목의 개수 1000개, 시간복잡도 : O(N), 제한시간 2초
  */
 public class Main1546 {
     public static void main(String[] args) {
@@ -18,21 +18,18 @@ public class Main1546 {
             scores[i] = sc.nextInt();
         }
 
-        // 최댓값 구하기
+        // 최댓값 구하면서 점수 총합 구하기
         double max = scores[0];
+        double sum = scores[0];
         for (int i = 1; i<scores.length; i++) {
             if (max < scores[i]) {
                 max = scores[i];
             }
+
+            sum += scores[i];
         }
 
-        // 점수 조작
-        double sum = 0;
-        for (int i = 0; i<scores.length; i++) {
-            sum += scores[i]/max*100;
-        }
-
-        System.out.println(sum/N);
+        System.out.println(sum / max * 100 / N);
         
         sc.close();
     }
