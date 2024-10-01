a01f3bf0d7b3b9bb956f92b6bcd35e72c8baf11e
5nam <ohnam90@gmail.com>
2024-10-01 19:40:37 +0900

    백준 10986 두 번째 풀이

--- list/Main10986.java
+++ list/Main10986.java
@@ -16,26 +16,28 @@ public class Main {
 
         // 합 배열 선언
         long s_arr[] = new long[n+1];
-        int count = 0;
 
-        // 합 배열 만들기
+        // 나머지 연산 수행한 합 배열 생성
         st = new StringTokenizer(bufferedReader.readLine());
         for(int i = 1; i<s_arr.length; i++) {
-            s_arr[i] = s_arr[i-1] + Integer.parseInt(st.nextToken());
+            s_arr[i] = (s_arr[i-1] + Integer.parseInt(st.nextToken())) % m;
         }
 
-        // m 으로 나누어지는 구간합 구하기
+        long count[] = new long[m];
+        // 나머지를 반영한 합 배열의 같은 값들 개수 세기
         for(int i = 1; i<s_arr.length; i++) {
-            for(int j = i; j<s_arr.length; j++) {
-                long num = s_arr[j] - s_arr[i-1];
-                if(num % m == 0) {
-                    count++;
-                }
-            }
+            count[(int)s_arr[i]]++;
         }
 
-        System.out.println(count);
+
+        // count 배열로 공통된 인덱스 수로 경우의 수 구하기
+        long result = count[0];
+        for(int i = 0; i<m; i++) {
+            // count[i] 개 중에 2개를 뽑는 경우의 수 계산 공식 count[i] * (count[i] - 1) / 2
+            result += count[i] * (count[i] - 1) / 2;
+        }
+
+        System.out.println(result);
         
     }
-    
 }
