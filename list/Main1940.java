82e06fbc3443ddc468dccafcfd933759e99859a6
5nam <ohnam90@gmail.com>
2024-10-03 11:00:23 +0900

    백준 1940 : 시간 복잡도 N 알고리즘 풀이, 투포인터 활용

---
+++ list/Main1940.java
@@ -0,0 +1,52 @@
+package list;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        // n 입력받기
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+
+        // m 입력받기
+        st = new StringTokenizer(br.readLine());
+        int m = Integer.parseInt(st.nextToken());
+
+        // nums(n 개의 재료들) 입력받고, 정렬
+        st = new StringTokenizer(br.readLine());
+        long nums[] = new long[n];
+        for(int i = 0; i<n; i++) {
+            nums[i] = Long.parseLong(st.nextToken());
+        }
+
+        Arrays.sort(nums);
+
+        // start, end, count 변수 초기화
+        int start = 0;
+        int end = n-1;
+        int count = 0;
+
+        while (start != end) {
+            long sum = nums[start] + nums[end];
+            if(sum > m) {
+                end--;
+            }
+            else if(sum < m) {
+                start++;
+            }
+            else {
+                count++;
+                start++;
+            }
+        }
+
+        System.out.println(count);
+
+    }
+}
