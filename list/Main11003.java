07deb08fbb76be1ba84cab642b6950d418525197
5nam <ohnam90@gmail.com>
2024-10-07 21:42:32 +0900

    알고리즘, 최솟값 찾기(11003) 풀이

---
+++ list/Main11003.java
@@ -0,0 +1,51 @@
+package list;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.StringTokenizer;
+
+public class Main11003 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        // 출력 버퍼에 넣고 한 번에 출력하기 위해 BufferedWriter 사용
+        StringTokenizer st = new StringTokenizer(bf.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int L = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(bf.readLine());
+        Deque<Node> deque = new LinkedList<>();
+
+        for(int i = 0; i<N; i++) {
+            int now = Integer.parseInt(st.nextToken());
+            // 새로운 값이 들어올 때마다 정렬 대신 현재 수보다 큰 값을 덱에서 제거해 시간 복잡도 줄임
+            while(!deque.isEmpty() && deque.getLast().value > now) {
+                deque.removeLast();
+            }
+            deque.addLast(new Node(now, i));
+
+            // 범위에서 벗어난 값은 덱에서 제거
+            if(deque.getFirst().index <= i - L) {
+                deque.removeFirst();
+            }
+            bw.write(deque.getFirst().value + " ");
+        }
+        bw.flush();
+        bw.close();
+    }
+
+    static class Node {
+        public int value;
+        public int index;
+
+        Node(int value, int index) {
+            this.value = value;
+            this.index = index;
+        }
+    }
+}
