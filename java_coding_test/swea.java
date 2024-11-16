937e301cdf9d0944416e8750fec19e713796adf0
5nam <ohnam90@gmail.com>
2024-11-16 23:59:23 +0900

    추가 풀이

--- java_coding_test/swea.java
+++ java_coding_test/swea.java
@@ -14,38 +14,30 @@ public class swea {
             nums[i] = Integer.parseInt(sc.next());
         }
 
-        int left = 0;
-        int right = n-1;
-        long total = 0;
-        long quantity = 0;
-        long purchase = 0;
-
-        while (true) {
-            if(left == right) {
-                total += quantity * nums[right] - purchase;
-                break;
-            }
-
-            if(nums[left] > nums[right]) {
-                if(quantity > 0) {
-                    total += quantity * nums[left] - purchase;
-                    quantity = 0;
-                    purchase = 0;
-                    left++;
-                }
-                else {
-                    right--;
-                }
-            }
-            else if(nums[left] < nums[right]) {
-                quantity++;
-                purchase += nums[left];
-                left++;
-            }
-        }
-
-        System.out.println("#" + test_case + " " + total);
-
-        sc.close();
+        int len = nums.length;
+			
+		//뒤에서부터 큰값이 나오면 리셋하기.맨뒤에 값이 무조건 큰값이라고 가정하기
+		long max = Long.MIN_VALUE;
+		int num = 0;
+		long cost = 0L;
+		long result = 0L;
+		for (int i = nums.length-1 ;  i >= 0 ; i--) {
+			// 더 큰 최댓값을 찾았다면? 호다닥 사자 ! 비용계산하기 : 사야할 애들의 갯수  * 판매가  - 구매가
+			if( nums[i] > max ) {
+				result += (max*num - cost);
+				max = nums[i];
+				//초기화
+				cost = 0;		
+				num = 0;
+			} else {	//max 보다 작으면 사자
+				cost+=nums[i];
+				num++;
+			}
+		}
+			
+		//마지막에 한번 더 해주기
+		result += (max*num - cost);
+			
+		System.out.println("#" + " " + result);
     }
 }
