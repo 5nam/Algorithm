4c0ee4007565d85abd82e851b03878cd81cab39d
5nam <ohnam90@gmail.com>
2024-11-20 20:51:14 +0900

    변경 사항 커밋

--- java_coding_test/Main9095.java
+++ java_coding_test/Main9095.java
@@ -2,7 +2,7 @@ package java_coding_test;
 
 import java.util.Scanner;
 
-public class Main {
+public class Main9095 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
