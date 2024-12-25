137467e1f017f98765c0844463c892ceb28e17b6
5nam <ohnam90@gmail.com>
2024-12-25 17:26:22 +0900

    브루트 포스 풀이 1. 맨 왼쪽에 있는 돌 선택 2. 그럼 방향이 총 5개로 한정됨. (위, 대각선 위, 오른쪽, 오른쪽 아래, 아래) 3. 해당 방향마다 탐색을 하기 위해 함수 생성 - 함수 로직 : 기준 돌(temp) 을 기준으로 해당 방향으로 갈 때마다 target 에 존재하면 cnt++(돌이 연속적으로 있다고 보는 것) : 연속적으로 있을 때까지만 cnt 를 증가시킴 : 그리고 그 개수가 5개이면서, 그 방향보다 먼저 있는 돌이 없는지 확인(그 방향으로 가는 반대로 가면 됨) : 있으면 바로 False 6돌은 오목 성립 안됨. : 없으면 바로 True. 4. 흑돌, 백돌 각각 실행한 뒤, 두 결과가 같으면 결과가 나오지 않은 것이므로 0, 다르면 True 인 사람이 이긴 것이므로 백돌, 흑돌 각각 체크해서 숫자 반환

--- EUREKA/practice.py
+++ EUREKA/practice.py
@@ -20,25 +20,79 @@ def solution(B, W):
     return answer
 
 def isWinner(target):
-    for k in range(len(target), 5-1, -1):
+    temp = sorted(target, key = lambda x : (x[0], x[1]))
+    print(temp)
 
-        combi = list(combinations(target, k))
+    for i in range(0, len(temp)):
+        result = search1(target, temp[i]) or search2(target, temp[i]) or search3(target, temp[i]) or search4(target, temp[i]) or search5(target, temp[i])
 
-        for i in range(0, len(combi)):
-            x = 0
-            y = 0
-            for j in range(0, 5):
-                x += combi[i][j][0]
-                y += combi[i][j][1]
+        if(result):
+            break
 
-            if (x%5==0) and (y%5 ==0) and (k==5):
-                return True
+    return result
+    
+
+def search1(target, temp):
+    cnt = 1
+    num = 1
+    while [temp[0]-num, temp[1]] in target:
+        cnt += 1
+        num += 1
+    
+    if cnt == 5 and [temp[0]+1, temp[1]] not in target:
+        return True
     
     return False
 
-
+def search2(target, temp):
+    cnt = 1
+    num = 1
+    while [temp[0]-num, temp[1]+num] in target:
+        cnt += 1
+        num += 1
     
+    if cnt == 5 and [temp[0]+1, temp[1]-1] not in target:
+        return True
     
+    return False
 
-# print(solution([[10,10],[11,10],[9,11],[11,11],[10,8],[12,13]], [[10,11],[9,10],[8,12],[11,12],[8,9],[7,8]]))
+def search3(target, temp):
+    cnt = 1
+    num = 1
+    while [temp[0], temp[1]+num] in target:
+        cnt += 1
+        num += 1
+    
+    if cnt == 5 and [temp[0], temp[1]-1] not in target:
+        return True
+    
+    return False
+
+def search4(target, temp):
+    cnt = 1
+    num = 1
+
+    while [temp[0]+num, temp[1]+num] in target:
+        cnt += 1
+        num += 1
+    
+    if cnt == 5 and [temp[0]-1, temp[1]-1] not in target:
+        return True
+    
+    return False
+
+def search5(target, temp):
+    cnt = 1
+    num = 1
+
+    while [temp[0]+num, temp[1]] in target:
+        cnt += 1
+        num += 1
+    
+    if cnt == 5 and [temp[0]-1, temp[1]] not in target:
+        return True
+    
+    return False
+
+print(solution([[10,10],[11,10],[9,11],[11,11],[10,8],[12,13]], [[10,11],[9,10],[8,12],[11,12],[8,9],[7,8]]))
 print(solution([[10,10],[10,11],[8,10],[9,10],[6,10],[11,10],[7,10]], [[9,11],[10,12],[11,13],[12,10],[5,10],[12,14]]))
