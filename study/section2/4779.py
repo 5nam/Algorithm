8e419ed308d7c54ec43787e4a025f18d68e4af77
5nam <ohnam90@gmail.com>
2025-01-01 18:32:13 +0900

    백준 4779 바텀업 방식으로 변경

--- study/section2/4779.py
+++ study/section2/4779.py
@@ -1,48 +1,12 @@
-def f(n):
-	global arr
-	global g
+ans = ['' for _ in range(13)]
+ans[0] = '-'
 
-	if n == 0:
-		return arr
-	for i in range(0, g, n):
-		if i % 2 == 0:
-			continue
-		for j in range(i, i+n):
-			arr[j] = False
-
-	return f(n//3)
-
-lines = []
-result = []
+for i in range(1, 13):
+	ans[i] = ans[i-1] + (' ' * (3**(i-1))) + ans[i-1]
+	
 while True:
-    try:
-        line = input()
-        
-        if line == "":  # 빈 줄이 입력되면 종료
-            break
-        
-        lines.append(int(line))
-
-    except EOFError:  # EOF를 만나면 반복 종료
-        break
-
-for i in lines:
-	n = i
-	g = 1
-
-	for i in range(n):
-		g = g*3
-
-	arr = [True] * g
-
-	result.append(f(g//3))
-
-
-for i in range(len(result)):
-	for j in range(len(result[i])):
-		if result[i][j]:
-			print("-", end="")
-		else:
-			print(" ", end="")
-
-	print()
+	try:
+		N = int(input())
+		print(ans[N])
+	except:
+		break
