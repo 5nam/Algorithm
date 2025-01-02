095f6b6f231f663ba9822818b790f83667b7effd
5nam <ohnam90@gmail.com>
2025-01-02 19:34:23 +0900

    백준 10974 다른 풀이에서 보고 보완 1. 출력함수 따로 만들지 않고, ' '.join(map(str, choose)) 이런식으로 join 활용하여 해결 2. n 에 따라 lst 를 만들고, 인덱스로 순열을 구했는데 이는 순차적으로 증가하는 수이므로 그냥 바로 1 ~ n 을 인자에 주면 되도록 변경, lst 사용 X

--- study/section2/10974.py
+++ study/section2/10974.py
@@ -1,23 +1,17 @@
 n = int(input())
 
-lst = list(i for i in range(1, n+1))
 choose = []
-check = [False] * n
-
-def prt(data):
-	for value in data:
-		print(value, end=" ")
-	print()
+check = [False] * (n+1)
 
 def permutation(level):
 	if level == n:
-		prt(choose)
+		print(' '.join(map(str, choose)))
 
-	for i in range(0, n):
+	for i in range(1, n+1):
 		if check[i]:
 			continue
 
-		choose.append(lst[i])
+		choose.append(i)
 		check[i] = True
 
 		permutation(level + 1)
