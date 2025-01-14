33beebf0aa5352a7f65a57eafe38b023e9e14b5f
5nam <ohnam90@gmail.com>
2025-01-14 10:56:30 +0900

    백준 2503 정답 문제 풀이

--- study/section2/2503.py
+++ study/section2/2503.py
@@ -2,32 +2,25 @@ from itertools import permutations
 
 n = int(input())
 
-inNum = [list(input().split()) for i in range(n)]
+inputNum = [input().split() for _ in range(n)]
 
-allNum = list(map(list, permutations([1,2,3,4,5,6,7,8,9], 3)))
+result = 0
 
-result = set()
+for cur in permutations(range(1, 10), 3):
+	for num, st, bl in inputNum:
+		ok = True
+		count_st = count_bl = 0
 
-for i in range(0, n):
-	num = list(map(int, inNum[i][0]))
-	
+		for i in range(3):
+			if str(cur[i]) == num[i]:
+				count_st += 1
+			elif str(cur[i]) in num: # str 타입으로 in 연산자 이용하여 문자열 중에 cur[i] 가 있는지 확인
+				count_bl += 1
 
-	for j in range(0, len(allNum)):
-		temp = list(allNum[j])
-		strike = 0
-		ball = 0
-		
-		for k in range(0, 3):
-			for l in range(0, 3):
-				if num[k] == temp[l]:
-					if k == l:
-						strike = strike+1
-					else:
-						ball = ball+1
+		if count_st != int(st) or count_bl != int(bl):
+			ok = False
+			break
 
-		if inNum[i][1] != strike or inNum[i][2] != ball:
-			print(temp)
-			result.add(tuple(temp)) # 지금 풀이가 틀린 이유. temp 의 인덱스와 result 의 인덱스가 일치하지 않으므로 엉뚱한 값을 지우는 것..
-			
+	result += ok # True 는 1 이고, False 는 0 이므로 이렇게 해도 되는 것
 
-print(len(allNum) - len(result))
+print(result)
