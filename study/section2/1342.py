19679d27c0014995b4972a017c4d8dc22ec75bd8
5nam <ohnam90@gmail.com>
2025-01-14 22:36:58 +0900

    백준 1342 백트레킹 풀이

--- study/section2/1342.py
+++ study/section2/1342.py
@@ -1,19 +1,38 @@
-from itertools import permutations
 
-inputStr = list(input())
-n = len(inputStr)
-result = set()
+def func(lev):
+	global chars, choose, cnt, s, result
 
-for target in permutations(inputStr, n):
-	ok = True
-	for i in range(n):
-		if i == n-1:
+	# base case
+	if lev == len(s):
+		result += 1
+		return
+
+	# recursive case
+	for c in chars:
+		if cnt[c] == 0:
 			continue
-		if target[i] == target[i+1]:
-			ok = False
-			break
-	
-	if ok:
-		result.add(tuple(target))
 
-print(len(result))
+		if (not choose) or (choose[-1] != c): # 되는 경우만 살펴보는 것
+			cnt[c] -= 1
+			choose.append(c)
+			func(lev+1)
+			cnt[c] += 1
+			choose.pop()
+
+s = input()
+chars = set()
+cnt = dict()
+
+for c in s:
+	chars.add(c) # s 에 있는 문자들 중복 없이 저장
+	if c not in cnt: # cnt 에 c 로 된 것 없으면 생성
+		cnt[c] = 0
+
+	cnt[c] += 1 # c 로 된 것 +1 해서 개수 세기
+
+choose = []
+result = 0
+
+func(0)
+
+print(result)
