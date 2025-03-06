df2d20f5f98eff3540be636a5c6c754cc2d889d9
5nam <ohnam90@gmail.com>
2025-03-06 21:02:58 +0900

    백준 12865 풀이

--- study/section2/12865.py
+++ study/section2/12865.py
@@ -1,26 +1,22 @@
 N, K = map(int, input().split())
-arr = list(list(map(int, input().split())) for _ in range(N)) 
 
-dp = [[[0, 0] for _ in range(N)] for _ in range(N)]
+# 무게와 가치를 담는 배열. 1인덱스로 사용할 것이므로 [0] 으로 선언해줌
+W = [0]
+V = [0]
 
-# 초기값 세팅
-for i in range(N):
-	dp[i][i][0] = arr[i][0]
-	dp[i][i][1] = arr[i][1] 
+for _ in range(N):
+	w, v = map(int, input().split())
+	W.append(w)
+	V.append(v)
 
-m_v = dp[0][0][1]
+# 초기값 0 으로 세팅 및 DP 테이블 생성
+dp = [[0 for _ in range(K+1)] for _ in range(N+1)]
+
 # dp 테이블 갱신
-for i in range(N):
-	for j in range(i+1, N):
-		if dp[i][j-1][0] + arr[j][0] <= K:
-			dp[i][j][0] = dp[i][j-1][0] + arr[j][0]
-			dp[i][j][1] = dp[i][j-1][1] + arr[j][1]
+for n in range(1, N+1):
+	for k in range(1, K+1):
+		dp[n][k] = dp[n-1][k]
+		if k - W[n] >= 0:
+			dp[n][k] = max(dp[n][k], dp[n-1][k-W[n]] + V[n])
 
-			if m_v < dp[i][j][1]:
-				m_v = dp[i][j][1]
-
-		else:
-			dp[i][j][0] = dp[i][j-1][0]
-			dp[i][j][1] = dp[i][j-1][1]
-
-print(m_v)
+print(dp[N][K])
