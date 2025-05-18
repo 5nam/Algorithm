a2b05f28981399dacb4edc6180ac49fd930bfa51
5nam <ohnam90@gmail.com>
2025-05-18 15:04:03 +0900

    백준 3085 알고리즘 N^3 풀이

--- study/section3/3085.py
+++ study/section3/3085.py
@@ -1,34 +1,37 @@
 # 가장 긴 공통 부분 찾는 알고리즘
-def get_best():
+def get_best(y, x):
 	global N, arr
 
 	best = 0
 	before = '-'
+	cnt = 0
 
-	for y in range(N):
-		cnt = 0
-		for x in range(N):
-			if before == arr[y][x]:
-				cnt +=1
+	# 행
+	for i in range(N):
+		for cur in arr[y][i]:
+			if before == cur:
+				cnt += 1
 			else:
 				cnt = 1
 
 			best = max(cnt, best)
-			before = arr[y][x]
+			before = cur
 
-	for x in range(N):
-		cnt = 0
-		for y in range(N):
-			if before == arr[y][x]:
-				cnt +=1
+	before = '-'
+	cnt = 0
+
+	# 열
+	for i in range(N):
+		for cur in arr[i][x]:
+			if before == cur:
+				cnt += 1
 			else:
 				cnt = 1
 
 			best = max(cnt, best)
-			before = arr[y][x]
+			before = cur
 
 	return best
-
 
 N = int(input())
 
@@ -37,24 +40,23 @@ arr = [list(input()) for _ in range(N)]
 dy = [1, 0, -1, 0]
 dx = [0, 1, 0, -1]
 
-# print(arr)
-
 best = 0
 
 # 교환 알고리즘
 for y in range(N):
 	for x in range(N):
-		temp = 0
+		# 모든 행과 열을 한 번씩은 봐야함.
+		# 교체 영향력 밖에 있는 행과 열이 최대 일 수 있으므로
+		if y == x:
+			best = max(get_best(y, x), best)
 		for i in range(4):
 			ny, nx = y+dy[i], x+dx[i]
-
 			if (0 <= ny < N) and (0 <= nx < N):
 				if arr[y][x] != arr[ny][nx]:
 					arr[y][x], arr[ny][nx] = arr[ny][nx], arr[y][x]
-					temp = get_best()
+					best = max(get_best(y, x), best)
 					arr[y][x], arr[ny][nx] = arr[ny][nx], arr[y][x]
 
-			best = max(temp, best)
 
 print(best)
 
