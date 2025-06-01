56c4623cdf09720bf0d3d4155c4389a5c3243d3a
5nam <ohnam90@gmail.com>
2025-06-01 09:16:54 +0900

    백준 1987 리스트 활용 문제 풀이

--- study/section4/1987.py
+++ study/section4/1987.py
@@ -1,24 +1,26 @@
 def search(y, x):
-	global R, C, arr, dy, dx, ans, st
+	global R, C, arr, dy, dx, ans, check, count
 
 	# base_case
 	if y < 0 or x < 0 or y >= R or x >= C:
 		return
-	if arr[y][x] in st:
+	if check[ord(arr[y][x]) - ord('A')]:
 		return
 
-	st.add(arr[y][x])
+	check[ord(arr[y][x]) - ord('A')] = True
+	count += 1
 
-	ans = max(ans, len(st))
+	ans = max(ans, count)
 
 	# recursive_base
 	for i in range(4):
-		ny = y + i
-		nx = x + i
+		ny = y + dy[i]
+		nx = x + dx[i]
 
 		search(ny, nx)
 
-	st.remove(arr[y][x])
+	check[ord(arr[y][x]) - ord('A')] = False
+	count -= 1
 
 
 # 입력받기
@@ -31,8 +33,9 @@ dy = [1, -1, 0, 0]
 dx = [0, 0, -1, 1]
 
 ans = 0
+count = 0
 
-st = set()
+check = [False] * 26
 
 search(0,0)
 
