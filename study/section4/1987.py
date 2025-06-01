66a934b3d12f3069db2f4db919f47191d3eaeacc
5nam <ohnam90@gmail.com>
2025-06-01 09:03:49 +0900

    백준 1987 set 활용 풀이

--- study/section4/1987.py
+++ study/section4/1987.py
@@ -1,37 +1,41 @@
+def search(y, x):
+	global R, C, arr, dy, dx, ans, st
+
+	# base_case
+	if y < 0 or x < 0 or y >= R or x >= C:
+		return
+	if arr[y][x] in st:
+		return
+
+	st.add(arr[y][x])
+
+	ans = max(ans, len(st))
+
+	# recursive_base
+	for i in range(4):
+		ny = y + i
+		nx = x + i
+
+		search(ny, nx)
+
+	st.remove(arr[y][x])
+
+
 # 입력받기
 R, C = map(int, input().split())
 
-# 둘이 뭐가 다른지 정리하기
-# arr = list('a' * C) + list('a' + input() for _ in range(R))
-arr = ['a' * C] + list('a' + input() for _ in range(R))
+arr = [input() for _ in range(R)]
 
 # 좌표 이동 설정(상하좌우)
 dy = [1, -1, 0, 0]
 dx = [0, 0, -1, 1]
 
-# dp 테이블
-dp = list([0] * (C+2) for _ in range(R+2))
+ans = 0
 
-# # dp 초기화
-# dp[1][1] = 1
+st = set()
 
-# 중복 값인지 체크하는 set 생성
-s_list = set()
-# s_list.add(arr[1][1])
+search(0,0)
 
-# dp 갱신
-for r in range(1, R+1):
-	for c in range(1, C+1):
-		if arr[r][c] not in s_list:
-			dp[r][c] = max(
-				dp[r+dy[0]][c+dx[0]],
-				dp[r+dy[1]][c+dx[1]],
-				dp[r+dy[2]][c+dx[2]],
-				dp[r+dy[3]][c+dx[3]]
-			) + 1
-			s_list.add(arr[r][c])
+print(ans)
 
-		else:
-			break
 
-print(dp)
