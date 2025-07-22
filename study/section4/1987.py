ae6dea0b89e6bdd5fadb9768d14e6bcc4aace8c1
5nam <ohnam90@gmail.com>
2025-07-22 19:37:09 +0900

    SWEA 1949 문제풀이

--- study/section4/1987.py
+++ study/section4/1987.py
@@ -1,44 +1,39 @@
-def search(y, x):
-	global R, C, arr, dy, dx, ans, check, count
+def search(r, c):
+	global ans, R, C, check, dir
 
-	# base_case
-	if y < 0 or x < 0 or y >= R or x >= C:
+	if r < 0 or r >= R or c < 0 or c >= C:
 		return
-	if check[ord(arr[y][x]) - ord('A')]:
+	
+	if check[ord(arr[r][c]) - 65]:
+		check[ord(arr[r][c]) - 65] = False
+		ans -= 1
 		return
+	
+	for d in range(4):
+		i, j = dir[d]
 
-	check[ord(arr[y][x]) - ord('A')] = True
-	count += 1
+		check[ord(arr[r][c]) - 65] = True
+		ans += 1
+		search(r+i, c+j)
+	
+	check[ord(arr[r][c]) - 65] = False
+	ans -= 1
+	return
 
-	ans = max(ans, count)
+# n, m 입력받기
+R, C = map(int, input().split(" "))
 
-	# recursive_base
-	for i in range(4):
-		ny = y + dy[i]
-		nx = x + dx[i]
-
-		search(ny, nx)
-
-	check[ord(arr[y][x]) - ord('A')] = False
-	count -= 1
-
-
-# 입력받기
-R, C = map(int, input().split())
-
+# arr 입력받기
 arr = [input() for _ in range(R)]
 
-# 좌표 이동 설정(상하좌우)
-dy = [1, -1, 0, 0]
-dx = [0, 0, -1, 1]
-
-ans = 0
-count = 0
+dir = [(1, 0), (-1, 0), (0, -1), (0, 1)]
 
 check = [False] * 26
 
-search(0,0)
+ans = 1
+
+for n in range(R):
+	for m in range(C):
+		search(n, m)
 
 print(ans)
-
-
